d4cf6db61f6225250d342e733ff58070dad7ed45
Jerry Nolazco <newyork_14@live.com>
2020-10-13 21:31:33 -0600

    Basic JavaScrpit

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,22 @@
+var D = "Testing JavaScript"
+
+window.alert (D)
+
+document.write ("Jane said \"I'm going outside\" to Jack over the phone.")
+
+document.write ("\"Computers are good at following instructions,"
++" but not at reading your mind\""
++" -Donald Knuth.")
+
+var K = " Let\'s get this" + " done!"
+document.write (K)
+
+var Cars = " Sport Cars", Subaru = " WRX STI", Honda = " Type R", Ford = " Cobra"; //Defining the varibles vaules
+var Cars = Cars.fontcolor("red"); //Giving "Cars" vaule a color
+var Subaru = Subaru.fontcolor("metallic grey"); //Defining Subaru's color
+var Honda = Honda.fontcolor("Green"); //Honda's color
+var Ford = Ford.fontcolor("Yellow"); //Ford's color
+
+document.write (Subaru); // Printing Subaru's value
+
+document.write ( 8+8)
