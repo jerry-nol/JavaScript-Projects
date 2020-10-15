a5e16b7d7bd2ce01c81c5117c38729092adf2968
Jerry Nolazco <newyork_14@live.com>
2020-10-14 19:41:15 -0600

    basic js project

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+var P ="Print these words.";
+var O = "These words will be grey";
+var O = O.fontcolor("grey") // O var will display grey
+document.write (O);
+
+function myFunction() {
+    var s = "learning JS"; // setting varible s a value
+    s += " is going to be tough"; // concatenating
+    document.getElementById("learn").innerHTML = s;
+}
